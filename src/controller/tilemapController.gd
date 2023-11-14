084c86d7d15e0f94c02a0f83a7614b7c76416e9a
AndiReich <andi.riele@gmail.com>
2023-11-14 22:39:08 +0100

    end of day commit

--- src/controller/tilemapController.gd
+++ src/controller/tilemapController.gd
@@ -28,20 +28,40 @@ func _ready():
 			grid_state[x].append(Vector2i.ZERO)
 
 func _on_virus_antigen_emanate(position : Vector2, type: Cell.TYPES):
-	print("Tries handling emanate.")
+	print_debug("Tries handling emanate.")
 	
 	
 	var local_position : Vector2 = to_local(position)
 	var map_position : Vector2i = local_to_map(local_position)
 	
+	print_debug("local coords = x: %d, y: %d" % [local_position.x, local_position.y])
+	print_debug("map coords   = x: %d, y: %d" % [map_position.x, map_position.y])
 	
-	print("local coords = x: %d, y: %d" % [local_position.x, local_position.y])
-	print("map coords   = x: %d, y: %d" % [map_position.x, map_position.y])
-	set_cell(0, Vector2i(map_position.x,map_position.y), 0, Vector2i(3,0))
-	print("Handled emit")
+	print_debug("Handled emit")
+	
+func set_pattern_by_cell_type(position: Vector2i, cell_type: Cell.TYPES):
+	if(!cell_pattern_dict.has(cell_type)):
+		print_debug("Pattern of %s not found." % cell_type)
+		return
+	var pattern_matrix = cell_pattern_dict[cell_type]
+	
+	var x_size = pattern_matrix.size
+	for x in x_size:
+		var y_size = pattern_matrix[x].size
+		for y in y_size:
+			var next_pos = Vector2i()
+			if(is_spot_viable(next_pos)):
+				print("")
+	print("")
 
-func preCheckPatternAgainstGrid():
-	print("test")
+func match_pattern_with_grid_state(pattern_matrix):
 	
-func setPatternForCell():
-	print("test")
+	return 
+	
+func is_spot_viable(next_pos: Vector2i) -> bool:
+	if(position.x < 0 || position.x > grid_state.size):
+		return false
+	if(position.y < 0 || position.y > grid_state[0].size):
+		return false
+	return true
+
