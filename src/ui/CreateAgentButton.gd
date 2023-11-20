7165d25c718891c50dff3f0ea3f61f76918adbe0
Arthur Grot <arthurgrot@gmx.de>
2023-11-20 20:18:35 +0100

    Prototype for agent placement. FPS and agent counter added.

---
+++ src/ui/CreateAgentButton.gd
@@ -0,0 +1,19 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _pressed():
+	var virus = preload("res://scenes/agents/virusTest.tscn").instantiate()
+	var agentRootNode = get_tree().root.find_child("AgentRootNode", true, false)
+	virus.initialCellType = Cell.TYPES.ANTIGEN
+	virus.isInitialize = true
+	agentRootNode.add_child(virus)
+	
