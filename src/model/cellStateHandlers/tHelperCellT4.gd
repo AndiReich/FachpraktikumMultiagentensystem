dbfceff222bd2295b94d477ca723f2a29fa20cca
FelixBangerter <felix.bangerter@cup.uni-muenchen.de>
2023-12-01 20:21:01 +0100

    Fixed sprite of T-helper cells.

--- src/model/cellStateHandlers/tHelperCellT4.gd
+++ src/model/cellStateHandlers/tHelperCellT4.gd
@@ -3,7 +3,7 @@ class_name THelperCellT4 extends CellStateHandler
 var cell_type: Cell.TYPES = Cell.TYPES.THELPERCELL
 
 func _init():
-	var base = Image.load_from_file("res://assets/cells/Antigen.png")
+	var base = Image.load_from_file("res://assets/cells/THelperCell.png")
 	var resulting_texture = ImageTexture.create_from_image(base)
 	cell_texture = resulting_texture
 
